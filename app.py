320fa169ff8a4647405c842ad596dde3f58f5be7
siva1403 <k.s.krishnajob@gmail.com>
2026-04-14 15:11:29 +0530

    t

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def home():
         "repository": os.getenv("GITHUB_REPOSITORY"),
         "branch": os.getenv("GITHUB_REF_NAME"),
         "commit_id": os.getenv("GITHUB_SHA"),
-        "timestamp": datetime.now(IST).strftime("%Y-%m-%d %H:%M:%S IST"),
+       # "timestamp": datetime.now(IST).strftime("%Y-%m-%d %H:%M:%S IST"),
         "message": "Flask app is running"
     }
     return jsonify(data)

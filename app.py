37c41541d75228e18160a426b5be2f463ea4afe0
siva1403 <k.s.krishnajob@gmail.com>
2026-04-14 15:05:08 +0530

    time

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    data = {
+        "repository": os.getenv("GITHUB_REPOSITORY"),
+        "branch": os.getenv("GITHUB_REF_NAME"),
+        "commit_id": os.getenv("GITHUB_SHA"),
+        "timestamp": datetime.ISTnow().strftime("%Y-%m-%d %H:%M:%S UTC"),
+        "message": "Flask app is running"
+    }
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(port=5000)

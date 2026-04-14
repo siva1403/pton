160fe23c48e249162f24d159671097d096639b75
siva1403 <k.s.krishnajob@gmail.com>
2026-04-14 15:16:47 +0530

    st

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
 from flask import Flask, jsonify
 import os
-from datetime import datetime
+from datetime import datetime, timezone
 
 app = Flask(__name__)
 
+def get_env(key):
+    return os.getenv(key, "unknown")
 
 @app.route("/")
 def home():
-    data = {
-        "repository": os.getenv("GITHUB_REPOSITORY"),
-        "branch": os.getenv("GITHUB_REF_NAME"),
-        "commit_id": os.getenv("GITHUB_SHA"),
-        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S IST"),
-        "message": "Flask app is runnig"
+    response = {
+        "status": "success",
+        "service": {
+            "name": "Flask App",
+            "version": "1.0.0"
+        },
+        "repository": {
+            "name": get_env("GITHUB_REPOSITORY"),
+            "branch": get_env("GITHUB_REF_NAME"),
+            "commit_id": get_env("GITHUB_SHA")
+        },
+        "runtime": {
+            "timestamp_utc": datetime.now(timezone.utc).isoformat(),
+            "environment": "production"
+        },
+        "message": "Flask app is running"
     }
-    return jsonify(data)
+
+    return jsonify(response)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(port=5000, debug=True)

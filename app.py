8f24021a1614a6ad65e343f40a5e4a9dcbfce7e7
siva1403 <k.s.krishnajob@gmail.com>
2026-04-14 15:13:23 +0530

    a

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, jsonify
 import os
 from datetime import datetime
-# import pytz
 
 app = Flask(__name__)
 
-IST = pytz.timezone("Asia/Kolkata")
 
 @app.route("/")
 def home():
@@ -13,7 +11,7 @@ def home():
         "repository": os.getenv("GITHUB_REPOSITORY"),
         "branch": os.getenv("GITHUB_REF_NAME"),
         "commit_id": os.getenv("GITHUB_SHA"),
-       # "timestamp": datetime.now(IST).strftime("%Y-%m-%d %H:%M:%S IST"),
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S IST"),
         "message": "Flask app is running"
     }
     return jsonify(data)
